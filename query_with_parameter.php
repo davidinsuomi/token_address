fcae34c3cb4d750fc731e2007a1eae7efe8d9c8d
weiding <davidinsuomi@gmail.com>
2017-09-10 11:04:26 +0800

    add table view

---
+++ query_with_parameter.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: weiding
+ * Date: 02/09/2017
+ * Time: 10:42
+ */
+require_once  'login.php';
+$token;
+if(isset($_GET['token'])){
+    $token = $_GET['token'];
+}else{
+    return;
+}
+$token = "stx_tbl";
+$conn = new mysqli($hn,$un,$pw,$db);
+if($conn->connect_error)
+    die($conn->connect_error);
+
+$sql = "SELECT * FROM ".$token;
+$result = $conn->query($sql);
+$data = array();
+if($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        //echo "num: " . $row["num_of_address"] . " Date: " . $row["time_stamp"]."<br>";
+        $a = array(strtotime($row["time_stamp"]),$row["num_of_address"]);
+        array_push($data,$a);
+    }
+}
+else{
+        echo "0 results";
+}
+
+echo json_encode($data);
+$conn->close();
+
+?>
+
